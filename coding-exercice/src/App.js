2a395090c18eac80869ed08ec71721c2d3ee7bc0
Khalid Esserhir <khalid.esserhir@gmail.com>
2021-04-20 17:56:00 +0000

    updated list pokemons

--- coding-exercice/src/App.js
+++ coding-exercice/src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import React, { useState, useRef, useCallback } from 'react'
 import './App.css';
+import ListPokemon from './components/ListPokemon'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
+ 
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+
+  const {
+    pokemons,
+    hasMore,
+    loading,
+    error
+  } = ListPokemon(query, pageNumber)
+
+  const observer = useRef()
+
+  const lastPokemonElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className="row">
+      <div className="col-md-6">
+      <h1>List Pokemons</h1>
+      <input type="text" className="form-control" value={query} onChange={handleSearch}></input>
+      <ul className="list-group">
+      {pokemons.map((pokemon, index) => {
+        if (pokemons.length === index + 1) {
+          return <li className="list-group-item"  ref={lastPokemonElementRef} key={pokemon}>{pokemon}
+          <button type="button" className="btn btn-success btn-lg d-flex">More Information</button>
+          </li>
+        } else {
+          return <li className="list-group-item" key={pokemon}>{pokemon}
+          <button type="button"  className="btn btn-success btn-lg d-flex">More Information</button>
+          </li>
+        }
+      })}
+      </ul>
+      <div>{loading && 'Loading...'}</div>
+      <div>{error && 'Error'}</div>
+    </div>
+    </div>
+    </div>
     </div>
   );
 }
---
+++ coding-exercice/src/components/ListPokemon.js
@@ -0,0 +1,40 @@
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
+
+ function ListPokemon(query, pageNumber) {
+    
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+    const [pokemons, setPokemons] = useState([])
+    const [hasMore, setHasMore] = useState(false)
+    useEffect(() => {
+        setPokemons([])
+      }, [query])
+    
+        useEffect(() => {
+            setLoading(true)
+            setError(false)
+            let cancel
+            axios({
+              method: 'GET',
+              url: 'https://pokeapi.co/api/v2/pokemon',
+              params: { q: query, page: pageNumber },
+              cancelToken: new axios.CancelToken(c => cancel = c)
+            }).then(res => {
+              setPokemons(prevPokemons => {
+                return [...new Set([...prevPokemons, ...res.data.results.map(p => p.name)])]
+              })
+              setHasMore(res.data.results.length > 0)
+              setLoading(false)
+            }).catch(e => {
+              if (axios.isCancel(e)) return
+              setError(true)
+            })
+            return () => cancel()
+          }, [query, pageNumber])
+        
+          return { loading, error, pokemons, hasMore }
+}
+
+export default ListPokemon
+
